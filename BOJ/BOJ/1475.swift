e35c0ee7ced9346ff89093facca1afef43db191b
chris69223 <chris6923@daum.net>
2021-04-20 18:47:17 +0900

    1475.swift

---
+++ BOJ/BOJ/1475.swift
@@ -0,0 +1,19 @@
+//
+//  1475.swift
+//  YoungSang
+//
+//  Created by 유저 on 2021/04/09.
+//
+
+import Foundation
+var num = Int(readLine()!)!
+var arr = [Int](repeating: 0, count: 9)
+
+for i in num{
+    arr[i] +=1
+}
+// 어캐 풀어야할지 모르겠네
+
+//readLine(!) map함수를 쓰면 string을 하나하나씩 받을 수 있어서 쉽다 오늘 푼 문제 다시 풀어보기!
+ 
+
