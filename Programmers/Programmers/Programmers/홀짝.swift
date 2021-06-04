b410d840bf74be9297ce542ecc7a55d6c6a137ba
chris69223 <chris6923@daum.net>
2021-06-04 20:57:40 +0900

    홀짝

---
+++ Programmers/Programmers/Programmers/홀짝.swift
@@ -0,0 +1,16 @@
+//
+//  홀짝.swift
+//  Programmers
+//
+//  Created by 유저 on 2021/06/04.
+//
+
+import Foundation
+
+func solution(_ num:Int) -> String {
+    if num % 2 == 0{
+        return "Even"
+    }else{
+        return "Odd"
+    }
+}
